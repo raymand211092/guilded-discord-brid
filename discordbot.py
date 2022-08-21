74d766817e3f9b8538329bb7c7b01190eda87d91
Charged <someonesomething800@gmail.com>
2022-08-21 18:39:10 -0400

    Add files.

---
+++ discordbot.py
@@ -0,0 +1,51 @@
+import discord
+from discord.ext import commands
+import os
+from dotenv import load_dotenv
+from discord_webhook import DiscordWebhook, DiscordEmbed
+# import urllib.request
+import re
+
+load_dotenv()
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix='W!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print('Discord bot logged in as ' + bot.user.name)
+    await bot.change_presence(activity=discord.Game(name=os.getenv('DISCORD_STATUS')))
+
+@bot.event
+async def on_message(ctx):
+    if str(ctx.webhook_id) == re.findall("discord.com\/api\/webhooks\/([^\/]+)\/", os.getenv('DISCORD_WEBHOOK'))[0]:
+        return
+    if str(ctx.channel.id) != os.getenv('DISCORD_CHANNEL_ID'):
+        return
+
+    displayname = ctx.author.name
+
+    try:
+        if ctx.author.nick:
+            displayname = ctx.author.nick + ' (' + ctx.author.name + ')'
+    except:
+        pass
+
+    webhook = DiscordWebhook(url=os.getenv('GUILDED_WEBHOOK'), content='<' + displayname + '> ' + ctx.content)
+    attachment_urls = []
+
+    if ctx.attachments:
+        for attachment in ctx.attachments:
+        # When Guilded supports file uploads, we can use this. For now, we'll leave it there.
+        #    req = urllib.request.Request(
+        #        attachment.url,
+        #        headers={'User-Agent':'DiscordBot (https://wiilink24.com, 1.0.0)'}
+        #    )
+        #    webhook.add_file(file=urllib.request.urlopen(req), filename=attachment.filename)
+            attachment_urls.append(attachment.url)
+        webhook = DiscordWebhook(url=os.getenv('GUILDED_WEBHOOK'), content='<' + displayname + '> ' + ctx.content + " " + " ".join(attachment_urls))
+
+    response = webhook.execute()
+
+bot.run(os.getenv('DISCORD_TOKEN'))
---
+++ guildedbot.py
@@ -0,0 +1,23 @@
+import guilded
+import os
+from dotenv import load_dotenv
+from discord_webhook import DiscordWebhook
+
+load_dotenv()
+
+bot = guilded.Client(command_prefix='!', owner_id='GmjJZnMm')
+
+@bot.event
+async def on_ready():
+    print('Guilded bot logged in as ' + bot.user.name)
+
+@bot.event
+async def on_message(ctx):
+        if ctx.author.name == 'Gil':
+            return
+        cleanedeveryone = ctx.content.replace('@everyone', '@​everyone')
+        cleanedhere = cleanedeveryone.replace('@here', '@​here')
+        webhook = DiscordWebhook(url=os.getenv('DISCORD_WEBHOOK'), content=cleanedhere, username=ctx.author.name)
+        response = webhook.execute()
+
+bot.run(os.getenv('GUILDED_TOKEN'))
